892708c6e6259ebb5247f79e7db8ebdea7b40a14
Peter Danis <peter.danis@outlook.com>
2021-03-15 16:59:29 +0100

    WIP on Jobs

--- src/components/Jobs/index.tsx
+++ src/components/Jobs/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Space, Table } from "antd";
+import { Button, Space, Table, Tag } from "antd";
 import Column from "antd/lib/table/Column";
 import React, { useEffect, useRef, useState } from "react";
 import fetchApi from "../../services/fetchApi";
@@ -22,28 +22,39 @@ type Job = {
   status: string;
 };
 
-type JobLog = {
-  id: number;
+type JobLog = Omit<Job, "schedule" | "runtimeResource"> & {
+  jobId: number;
   scheduleId: number;
   runtimeResourceId: number;
-  status: string;
 };
 
 const Jobs = () => {
   const [jobs, setJobs] = useState([] as Job[]);
+  const [jobLogs, setJobLogs] = useState([] as JobLog[]);
   const [filteredJobs, setFilteredJobs] = useState([] as Job[]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [filteredJobLogs, setFilteredJobLogs] = useState([] as JobLog[]);
+  const [isLoadingJobs, setIsLoadingJobs] = useState(true);
+  const [isLoadingJobLogs, setIsLoadingJobLogs] = useState(true);
   // const [selectedJobId, setSelectedJobId] = useState(0);
 
   // const AddUserModalRef = useRef<{ showModal: () => void }>(null);
   // const DeleteUserModalRef = useRef<{ showModal: () => void }>(null);
 
   const loadData = () => {
-    setIsLoading(true);
+    setIsLoadingJobs(true);
     fetchApi("/api/jobs")
       .then((data) => {
-        setIsLoading(false);
+        setIsLoadingJobs(false);
         setJobs(data);
+      })
+      .catch((error) => {
+        openNotification("Error", error.message, "error");
+        console.error(error);
+      });
+    fetchApi("/api/jobLogs")
+      .then((data) => {
+        setIsLoadingJobLogs(false);
+        setJobLogs(data);
       })
       .catch((error) => {
         openNotification("Error", error.message, "error");
@@ -73,14 +84,19 @@ const Jobs = () => {
         </Button> */}
         <SearchBox
           list={jobs}
-          keys={["id", "name"]}
-          placeholder="Search by ID or name"
+          keys={[
+            "schedule.id",
+            "schedule.name",
+            "runtimeResource.friendlyName",
+            "status",
+          ]}
+          placeholder="Search by schedule, machine, status..."
           resultsSetter={setFilteredJobs}
         />
         <Table
           dataSource={filteredJobs}
           rowKey={(record: any) => record.id}
-          loading={isLoading}
+          loading={isLoadingJobs}
           size="middle"
           sticky={true}
         >
@@ -88,6 +104,55 @@ const Jobs = () => {
           <Column title="Priority" dataIndex="priority" />
           <Column title="Machine" dataIndex="runtimeResource.friendlyName" />
           <Column title="Schedule" dataIndex="schedule.name" />
+          <Column
+            title="Status"
+            dataIndex="status"
+            render={(job) => {
+              console.log(job);
+              return (
+                <Tag color="red" key={Math.random()}>
+                  {job}
+                </Tag>
+              );
+            }}
+          />
+          {/* <Column
+            title="Actions"
+            dataIndex="actions"
+            render={(_, record: User) => (
+              <>
+                <Space>
+                  <Button size="small">Generate API key</Button>
+                  <Button
+                    size="small"
+                    danger
+                    icon={<DeleteOutlined />}
+                    onClick={(e) => {
+                      setSelectedJobId(record.id);
+                      DeleteUserModalRef?.current?.showModal();
+                    }}
+                  />
+                </Space>
+              </>
+            )}
+          /> */}
+        </Table>
+        <SearchBox
+          list={jobLogs}
+          keys={["jobId", "status"]}
+          placeholder="Search by job ID, status"
+          resultsSetter={setFilteredJobLogs}
+        />
+        <Table
+          dataSource={filteredJobLogs}
+          rowKey={(record: any) => record.id}
+          loading={isLoadingJobLogs}
+          size="middle"
+          sticky={true}
+        >
+          <Column title="Priority" dataIndex="priority" />
+          <Column title="Machine" dataIndex="runtimeResourceId" />
+          <Column title="Schedule" dataIndex="scheduleId" />
           {/* <Column
             title="Actions"
             dataIndex="actions"
